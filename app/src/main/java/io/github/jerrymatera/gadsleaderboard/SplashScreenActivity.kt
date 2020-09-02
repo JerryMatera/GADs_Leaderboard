05f784128693908a8fb8dd1143fc8e5b121ac43b
JerryMatera <jerryonyango.95@gmail.com>
2020-09-02 23:01:57 +0300

    Set up Splash Screen

---
+++ app/src/main/java/io/github/jerrymatera/gadsleaderboard/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package io.github.jerrymatera.gadsleaderboard
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private  val splashTime = 3000L
+    lateinit var myHandler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        myHandler = Handler()
+
+        myHandler.postDelayed({
+            goToMainActivity()
+        }, splashTime)
+    }
+
+    private fun goToMainActivity() {
+        val mainActivityIntent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(mainActivityIntent)
+        finish()
+    }
+}
